79c30bebfa1e30214cb7afb681bbdfcfa15d9d2f
lalasanchez99 <arualzehcnas17@gmail.com>
2022-01-28 23:30:43 -0500

    first commit

---
+++ ejercicio01.js
@@ -0,0 +1,15 @@
+
+let nombre =prompt("Ingresa tu nombre");
+alert("Bienvenida " + nombre);
+let pantalla = prompt("Ingresa el numero de tu pantalla");
+let clave = prompt(nombre + " Ingresa tu clave por favor");
+
+if (pantalla == 1 && clave == 2022) {
+  alert("Continuar viendo One Piece");
+} else {
+  alert("Esta pantalla no te pertenece impostor");
+}
+
+datos.onclick = function () {
+  nombre();
+};
